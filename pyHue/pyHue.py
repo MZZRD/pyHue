522408fe279e9c071ff552d50ecaa38516d225c3
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2020-12-17 18:06:42 +0100

    Adding Light class with toggle function

--- pyHue/pyHue.py
+++ pyHue/pyHue.py
@@ -1,6 +1,7 @@
 
 import requests
 import time
+from helpers import Light
 
 
 def get_bridge_info():
@@ -38,13 +39,6 @@ def create_new_user(bridge_ip, user):
 
     return 0
 
-
-def toggle_light(url, light):
-    body = '{"on":true}'
-    r = requests.put("%s/lights/3/state" % url, body)
-    print(r.json()[0])
-
-
 def main():
     # # get bridge information
     # bridge_info = get_bridge_info()
@@ -53,8 +47,20 @@ def main():
     # bridge_ip = bridge_info.get('internalipaddress')
     # user_name = create_new_user(bridge_ip, "mjmtv")
 
-    url = "http://192.168.1.2/api/zxf8uotr3FIGEl2G3msqRfseTlEZeS0wOrMbOE2p"
-    toggle_light(url,3)
+    # url = "http://192.168.1.2/api/zxf8uotr3FIGEl2G3msqRfseTlEZeS0wOrMbOE2p"
+
+    # for i in range(100):
+    #     toggle_light(url, 3)
+    #     time.sleep(0.5)
+
+    # toggle_light(url, 3)
+
+    url = "http://192.168.1.2/api/zxf8uotr3FIGEl2G3msqRfseTlEZeS0wOrMbOE2p/lights/3"
+    kitchen = Light(url)
+
+    print(kitchen.on)
+    kitchen.toggle()
+    print(kitchen.on)
 
 
 if __name__ == '__main__':
